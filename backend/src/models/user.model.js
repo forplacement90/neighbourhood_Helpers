4c84b6cbe6e6c6f7eb7ae3b3783f5c9373b96617
sourav kumar <souravkumar85055@gmail.com>
2025-07-13 20:32:16 +0530

    edit user model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -70,7 +70,8 @@ userSchema.methods.generateAccessToken = function(){
             _id: this._id,
             email: this.email,
             username: this.username,
-            fullName: this.fullName
+            fullName: this.fullName,
+            team_role:this.team_role
         },
         process.env.ACCESS_TOKEN_SECRET,
         {
